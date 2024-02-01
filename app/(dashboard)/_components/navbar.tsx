1675b678e64d6e6dcea7c708bd87dc84cb144bab
AnantPrabhudesai01 <AnantPrabhudesai444@gmail.com>
2024-02-01 21:25:40 +0530

    Navbar Done

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,14 +1,54 @@
 "use client"
 
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton, useOrganization } from '@clerk/nextjs';
+import SearchInput from './search-input';
+import { InviteButton } from './invite-button';
 
 export const Navbar = () => {
+const {organization} = useOrganization();
     return (
-        <header className="flex items-center gap-x-4 p-5 bg-[#9f84ff]/50">
+        <header className="flex items-center gap-x-4 p-5">
             Navbar
-            <div className="hidden lg:flex lg:flex-1 bg-yellow-400/60">
-                Search
+            <div className="hidden lg:flex lg:flex-1 ">
+                
+                {/* Only visible on the Desktop Size */}
+                <SearchInput/>
             </div>
+            <div
+            className="block lg:hidden flex-1" 
+            >
+                <OrganizationSwitcher
+        //hidePersonal wil remove the personal organiztion
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              maxWidth: "376px",
+            },
+            //Below is for the Border
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white",
+            },
+          },
+        }}
+      />
+
+            </div>
+
+            {
+                organization && (
+                    <InviteButton />
+                )   
+            }
             <UserButton />
         </header>
     )
--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -12,101 +11,78 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
 const font = Poppins({
-    subsets: ["latin"],
-    weight: ["600"],
-  });
-  
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export const OrgSidebar = () => {
-    const searchParams = useSearchParams();
-    const favorites = searchParams.get("favorites");
+  const searchParams = useSearchParams();
+  const favorites = searchParams.get("favorites");
 
-    return (
-        <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-[#6bffdf]/70">
-            <Link href="">
-                <div className="flex items-center gap-x-2">
-                    <Image
-                    src="/logo.svg"
-                    alt="logo"
-                    width={60}
-                    height={60}
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-[#6bffdf]/70">
+      <Link href="">
+        <div className="flex items-center gap-x-2">
+          <Image src="/logo.svg" alt="logo" width={60} height={60} />
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Board
+          </span>
+        </div>
+      </Link>
 
-                    />
-                        <span
-                        className={cn(
-                            "font-semibold text-2xl",
-                            font.className,
-                        )}
-                        >
-                            Board
-                        </span>
-                </div>
-            </Link>
-
-        {/* we can switch the orginazation */}
-            <OrganizationSwitcher
-//hidePersonal wil remove the personal organiztion 
-hidePersonal
-            appearance={{
-                elements:{
-                    rootBox:{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "100%",
-                    
-                    },
-                    //Below is for the Border
-                    organizationSwitcherTrigger:{
-                        padding: "6px",
-                        width: "100%",
-                        borderRadius: "8px",
-                        border: "1px solid #E5E7EB",
-                        justifyContent: "space-between",
-                        backgroundColor: "white",
-                    }
-
-                }
-            }}
-            />
-        <div
-        className="space-y-1 w-full"
+      {/* we can switch the orginazation */}
+      <OrganizationSwitcher
+        //hidePersonal wil remove the personal organiztion
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            },
+            //Below is for the Border
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white",
+            },
+          },
+        }}
+      />
+      <div className="space-y-1 w-full">
+        <Button
+          variant={favorites ? "ghost" : "secondary"}
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
         >
-            <Button
-            variant={favorites ? "ghost" : "secondary"}
-            asChild
-            size="lg"
-            className="font-normal justify-start px-2 w-full"
-            >
-                <Link href="/">
-                <LayoutDashboard
-                className="h-4 w-4 mr-2"
-                />
-                </Link>
-                
-            </Button>
-            <Button
-            variant={favorites ? "secondary" : "ghost"}
-            asChild
-            size="lg"
-            className="font-normal justify-start px-2 w-full"
-            >
-                <Link href={{
-                    pathname: "/",
-                    query: {favorites: true}
-                }}
-                >
-                <LayoutDashboard
-                className="h-4 w-4 mr-2"
-                />
-                </Link>
-                <Star
-                className="h-4 w-4 mr-2"  
-                />
-                Fav Boards
-                
-            </Button>
-        </div>
-
-        </div>
-    )
-}
+          <Link href="/">
+            <LayoutDashboard className="h-4 w-4 mr-2" />
+          </Link>
+        </Button>
+        <Button
+          variant={favorites ? "secondary" : "ghost"}
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link
+            href={{
+              pathname: "/",
+              query: { favorites: true },
+            }}
+          >
+            <LayoutDashboard className="h-4 w-4 mr-2" />
+          </Link>
+          <Star className="h-4 w-4 mr-2" />
+          Fav Boards
+        </Button>
+      </div>
+    </div>
+  );
+};
