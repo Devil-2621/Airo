5c7dea9916a9f5c5c8a6a6fab553aeda67716316
Tirth Ladani <tirth8000@gmail.com>
2024-02-04 21:43:52 +0530

    Room Participants Done

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,56 @@
+'use client';
+
+import { UserAvatar } from './user-avatar';
+import { useOthers, useSelf } from '@/liveblocks.config';
+
 import { Skeleton } from '@/components/ui/skeleton';
+import { connectionIdToColor } from '@/lib/utils';
+
+const MAX_SHOW_USER = 2;
 
 export const Participants = () => {
+	const users = useOthers();
+	const currentUser = useSelf();
+	const hasMoreUsers = users.length > MAX_SHOW_USER;
+	
 	return (
 		<div className='absolute h-12 top-2 right-2 bg-white rounded-md p-2 flex items-center shadow-md'>
-			List of users
+			<div className='flex gap-x-2'>
+				{ users.slice(0, MAX_SHOW_USER)
+					.map(({ connectionId, info }) => {
+						return (
+							<UserAvatar
+								key={ connectionId }
+								src={ info?.picture }
+								name={ info?.name }
+								fallback={ info?.name?.[0] || "T" }
+								borderColor={connectionIdToColor(connectionId)}
+							/>
+						)
+					}) }
+				
+				{
+					currentUser && (
+						<UserAvatar
+							src={ currentUser.info.picture }
+							name={ `${currentUser.info?.name} (You)` }
+							fallback={ currentUser.info?.name?.[0] }
+							borderColor={connectionIdToColor(currentUser.connectionId)}
+						/>
+					)
+				}
+
+				{
+					hasMoreUsers && (
+						<UserAvatar
+							src=''
+							name={ `${users.length - MAX_SHOW_USER} more` }
+							fallback={ `+${users.length - MAX_SHOW_USER}` }
+							borderColor=''
+						/>
+					)
+				}
+			</div>
 		</div>
 	);
 };
