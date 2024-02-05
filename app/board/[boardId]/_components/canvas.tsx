b093c55ca85b73a5926fd6a262de79db05e5b4de
AnantPrabhudesai01 <AnantPrabhudesai444@gmail.com>
2024-02-05 12:26:59 +0530

    Canvas State Done

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useState } from 'react';
+import {CanvasMode,CanvasState} from "../../../../types/canvas";
 import React from 'react'
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
 import { Status } from '../_components/status';
 import { LostConnectionToasts } from '../_components/lost-connection-toast';
+import { useHistory,useCanUndo,useCanRedo } from '@/liveblocks.config';
 
 interface CanvasProps {
     boardId: string;
@@ -14,6 +17,15 @@ interface CanvasProps {
 export const Canvas = ({
   boardId,
 }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
+
   return (
     <main
     className='h-full w-full relative bg-neutral-100 touch-none'
@@ -23,7 +35,15 @@ export const Canvas = ({
       <div className='absolute right-2 bottom-3'>
       <Status />
       </div>
-      <Toolbar />
+      <Toolbar 
+      canvasState={canvasState}
+      setCanvasState={setCanvasState}
+      undo={history.undo}
+      redo={history.redo}
+      canUndo={canUndo}
+      canRedo={canRedo}
+
+      />
       <LostConnectionToasts />
     </main>
   )
